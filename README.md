db40d2bad618c0fb34a9922ba7e719fd43d1e847
Janez Urevc <janez@janezurevc.name>
2020-07-15 10:40:05 +0200

    Project structure.

---
+++ README.md
@@ -0,0 +1,21 @@
+# BsvP2p
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `bsv_p2p` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:bsv_p2p, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/bsv_p2p](https://hexdocs.pm/bsv_p2p).
+
---
+++ mix.exs
@@ -0,0 +1,35 @@
+defmodule BsvP2p.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bsv_p2p,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {BsvP2p.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_check, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:sobelow, ">= 0.0.0", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:connection, "~> 1.0.4"},
+      {:bsv, "~> 0.2.6"},
+    ]
+  end
+end
