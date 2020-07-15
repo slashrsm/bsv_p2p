e16ac7afd2c4dc0bf6c394358866925678a16f9a
Janez Urevc <janez@janezurevc.name>
2020-07-16 00:08:20 +0200

    Implement services flag parsing.

--- lib/bsv_p2p/command/version.ex
+++ lib/bsv_p2p/command/version.ex
@@ -3,12 +3,18 @@ defmodule BsvP2p.Command.Version do
   Bitcoin P2P "version" command.
   """
   alias BSV.Util
+  use Bitwise
 
   @version Mix.Project.config()[:version]
+  @service_bits %{
+    0x01 => :node_network,
+    0x02 => :node_getutxo,
+    0x04 => :node_bloom,
+    0x400 => :node_network_limited
+  }
 
   defstruct version: 70_015,
-            # TODO
-            services: <<0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00>>,
+            services: [:node_network],
             nonce: BSV.Util.random_bytes(8),
             timestamp: DateTime.utc_now(),
             user_agent: "/bsv_p2p:#{@version}/",
@@ -17,9 +23,11 @@ defmodule BsvP2p.Command.Version do
             # TODO determine automatically.
             sender: nil
 
+  @type node_service :: :node_netowrk | :node_getutxo | :node_bloom | :node_network_limited
+
   @type t :: %__MODULE__{
           version: non_neg_integer,
-          services: binary,
+          services: [node_service],
           nonce: binary,
           timestamp: DateTime.t(),
           user_agent: String.t(),
@@ -36,8 +44,10 @@ defmodule BsvP2p.Command.Version do
 
     @spec get_payload(BsvP2p.Command.Version.t()) :: binary
     def get_payload(%BsvP2p.Command.Version{} = command) do
+      services_payload = BsvP2p.Command.Version.get_services_payload(command)
+
       <<command.version::integer-unsigned-size(32)-little>> <>
-        command.services <>
+        <<services_payload::integer-unsigned-size(64)-little>> <>
         <<DateTime.to_unix(command.timestamp)::integer-unsigned-size(64)-little>> <>
         <<0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00>> <>
         <<0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x7F, 0x00,
@@ -57,7 +67,7 @@ defmodule BsvP2p.Command.Version do
 
   @spec from_payload(binary) :: BsvP2p.Command.Version.t()
   def from_payload(
-        <<version::integer-unsigned-size(32)-little, services::binary-size(8),
+        <<version::integer-unsigned-size(32)-little, services::integer-unsigned-size(64)-little,
           timestamp::integer-unsigned-size(64)-little, _recipient::binary-size(26),
           _sender::binary-size(26), nonce::binary-size(8), rest::binary>>
       ) do
@@ -65,7 +75,7 @@ defmodule BsvP2p.Command.Version do
 
     %__MODULE__{
       version: version,
-      services: services,
+      services: services_from_payload(services),
       nonce: nonce,
       timestamp: DateTime.from_unix!(timestamp),
       recipient: nil,
@@ -73,5 +83,33 @@ defmodule BsvP2p.Command.Version do
       user_agent: user_agent,
       latest_block: latest_block
     }
+  end
+
+  @spec get_services_payload(__MODULE__.t()) :: non_neg_integer
+  def get_services_payload(%BsvP2p.Command.Version{services: services}) do
+    do_services_payload(services, 0)
+  end
+
+  @spec do_services_payload([node_service], non_neg_integer) :: non_neg_integer
+  defp do_services_payload([:node_network | other_services], payload),
+    do: do_services_payload(other_services, payload ||| 1)
+
+  defp do_services_payload([:node_getutxo | other_services], payload),
+    do: do_services_payload(other_services, payload ||| 2)
+
+  defp do_services_payload([:node_bloom | other_services], payload),
+    do: do_services_payload(other_services, payload ||| 4)
+
+  defp do_services_payload([:node_network_limited | other_services], payload),
+    do: do_services_payload(other_services, payload ||| 1024)
+
+  defp do_services_payload([], payload), do: payload
+
+  @spec services_from_payload(non_neg_integer) :: [node_service]
+  def services_from_payload(payload) do
+    @service_bits
+    |> Enum.map(fn {bits, service} -> {bits &&& payload, service} end)
+    |> Enum.filter(fn {present, _service} -> present > 0 end)
+    |> Enum.map(fn {_, service} -> service end)
   end
 end
--- test/bsv_p2p/command/version_test.exs
+++ test/bsv_p2p/command/version_test.exs
@@ -4,11 +4,11 @@ defmodule BsvP2p.Command.VersionTest do
   alias BsvP2p.Command.Version
   doctest BsvP2p.Command.Version
 
-  @test_payload <<205, 129, 1, 0, 1, 2, 255, 238, 170, 187, 17, 34, 97, 188, 102, 73, 0, 0, 0, 0,
-                  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 127, 0, 0, 1,
-                  32, 141, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 127, 0,
-                  0, 1, 32, 141, 1, 2, 3, 4, 5, 6, 7, 8, 15, 47, 98, 115, 118, 95, 112, 50, 112,
-                  58, 48, 46, 48, 46, 48, 47, 241, 251, 9, 0>>
+  @test_payload <<205, 129, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 97, 188, 102, 73, 0, 0, 0, 0, 1, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 127, 0, 0, 1, 32, 141, 1,
+                  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 127, 0, 0, 1, 32,
+                  141, 1, 2, 3, 4, 5, 6, 7, 8, 15, 47, 98, 115, 118, 95, 112, 50, 112, 58, 48, 46,
+                  48, 46, 48, 47, 241, 251, 9, 0>>
 
   test "Command.name/1" do
     assert Command.name(%Version{}) == "version"
@@ -17,7 +17,7 @@ defmodule BsvP2p.Command.VersionTest do
   test "Command.get_payload/1" do
     version = %Version{
       version: 98_765,
-      services: <<0x01, 0x02, 0xFF, 0xEE, 0xAA, 0xBB, 0x11, 0x22>>,
+      services: [:node_network],
       nonce: <<0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08>>,
       timestamp: ~U[2009-01-09 02:54:25Z],
       user_agent: "/bsv_p2p:0.0.0/",
@@ -29,10 +29,10 @@ defmodule BsvP2p.Command.VersionTest do
     assert @test_payload == Command.get_payload(version)
   end
 
-  test "BsvP2p.Command.Verack.from_payload/1" do
+  test "BsvP2p.Command.Version.from_payload/1" do
     assert %Version{
              version: 98_765,
-             services: <<0x01, 0x02, 0xFF, 0xEE, 0xAA, 0xBB, 0x11, 0x22>>,
+             services: [:node_network],
              nonce: <<0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08>>,
              timestamp: ~U[2009-01-09 02:54:25Z],
              user_agent: "/bsv_p2p:0.0.0/",
@@ -40,5 +40,45 @@ defmodule BsvP2p.Command.VersionTest do
              recipient: nil,
              sender: nil
            } == Version.from_payload(@test_payload)
+  end
+
+  test "BsvP2p.Command.Version.get_services_payload/1" do
+    command = %Version{
+      version: 98_765,
+      services: [:node_network],
+      nonce: <<0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08>>,
+      timestamp: ~U[2009-01-09 02:54:25Z],
+      user_agent: "/bsv_p2p:0.0.0/",
+      latest_block: 654_321,
+      recipient: nil,
+      sender: nil
+    }
+
+    assert 1 == Version.get_services_payload(command)
+    assert 2 == Version.get_services_payload(%{command | services: [:node_getutxo]})
+    assert 4 == Version.get_services_payload(%{command | services: [:node_bloom]})
+    assert 1024 == Version.get_services_payload(%{command | services: [:node_network_limited]})
+
+    assert 1031 ==
+             Version.get_services_payload(%{
+               command
+               | services: [:node_network, :node_getutxo, :node_bloom, :node_network_limited]
+             })
+
+    assert_raise FunctionClauseError, fn ->
+      Version.get_services_payload(%{command | services: [:foo]})
+    end
+  end
+
+  test "BsvP2p.Command.Version.services_from_payload/1" do
+    assert [:node_network] == Version.services_from_payload(1)
+    assert [:node_getutxo] == Version.services_from_payload(2)
+    assert [:node_bloom] == Version.services_from_payload(4)
+    assert [:node_network_limited] == Version.services_from_payload(1024)
+
+    assert [:node_network, :node_getutxo, :node_bloom, :node_network_limited] ==
+             Version.services_from_payload(1031)
+
+    assert [:node_network, :node_getutxo, :node_bloom] == Version.services_from_payload(0xFF)
   end
 end
