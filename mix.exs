f9bef644641987e31e2cc3ed8204b2ac95c00a7d
Janez Urevc <janez@janezurevc.name>
2020-07-21 11:43:37 +0200

    Remove old LICENSE file, use forked :bsv.

--- mix.exs
+++ mix.exs
@@ -49,8 +49,7 @@ defmodule BsvP2p.MixProject do
       {:mock, "~> 0.3.5", only: :test},
       {:excoveralls, "~> 0.10", only: :test},
       {:connection, "~> 1.0.4"},
-      {:bsv, "~> 0.2.6"},
-      #{:bsv, github: "slashrsm/bsv-ex"}
+      {:bsv, github: "slashrsm/bsv-ex"}
     ]
   end
 
